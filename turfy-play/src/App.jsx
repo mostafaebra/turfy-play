34f246b6c6fc1da82213b5e91fd8e50515bf1f4d
Mostafa Ebraheem <MostafaEbraheem@gmail.com>
2025-12-02 20:37:46 +0200

    our seconed structure

--- turfy-play/src/App.jsx
+++ turfy-play/src/App.jsx
@@ -1,10 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Import your pages
+import Home from './pages/home/home.jsx';
+import Login from './pages/login/login.jsx';
+import SignUp from './pages/signup/signup.jsx';
 
 function App() {
   return (
-    <div>
-      <h1>welcome home champ you can do it ❤️❤️</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        {/* The "index" route - what you see at http://localhost:5173/ */}
+        <Route path="/" element={<Home />} />
+
+        {/* Auth Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* Catch-all: Redirect unknown URLs back to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+
+);
 }
+
 
 export default App;
