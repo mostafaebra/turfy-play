c0a4bea9777d28929da17a18df8dc7f3a9394a87
Ahmed Gomaa <ahmedgomaa1289@gmail.com>
2025-12-03 14:24:17 +0200

    chore: setup tailwind css and initial folder structure

--- turfy-play/src/App.jsx
+++ turfy-play/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // Import your pages
-import Home from './pages/home/home.jsx';
-import Login from './pages/login/login.jsx';
-import SignUp from './pages/signup/signup.jsx';
+import Home from "./pages/home/home.jsx";
+import Login from "./pages/login/login.jsx";
+import SignUp from "./pages/signup/signup.jsx";
+import AddNewField from "./pages/addNewField/AddNewField.jsx";
 
 function App() {
   return (
@@ -16,14 +17,13 @@ function App() {
         {/* Auth Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/add-field" element={<AddNewField />} />
 
         {/* Catch-all: Redirect unknown URLs back to Home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-
-);
+  );
 }
-
 
 export default App;
