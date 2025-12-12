bd5882fae84e0250b4cc6b3ee14b236b5ca2f1f1
yomna <yomna7250@gmail.com>
2025-12-13 01:53:56 +0200

    Owner : Signup & LoginIn

--- owner/src/App.jsx
+++ owner/src/App.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 import AddNewField from "./pages/addNewField/AddNewField.jsx";
+import Ownerlogin from "./pages/Ownerlogin/Ownerlogin.jsx";
+import OwnerSignUp from "./pages/Ownersignup/Ownersignup.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* The "index" route - what you see at http://localhost:5173/ */}
-        <Route path="addNewField/AddNewField.jsx" element={<AddNewField />} />
+        <Route path="addNewField/AddNewField.jsx" element={<AddNewField />} /> 
 
+           
         {/* Auth Routes */}
        
         <Route path="/" element={<AddNewField />} />
+        <Route path="/Ownerlogin" element={<Ownerlogin/>} />
+         <Route path="/Ownersignup" element={<OwnerSignUp/>} />
 
         {/* Catch-all: Redirect unknown URLs back to Home */}
         <Route path="*" element={<Navigate to="/" replace />} />
---
+++ owner/src/pages/Ownerlogin/Ownerlogin.jsx
@@ -0,0 +1,19 @@
+import OwnerloginHeader from "../../components/OwnerloginHeader";
+import OwnerloginForm from "../../components/Ownerloginform";
+import OwnerloginFooter from "../../components/OwnerloginFooter";
+
+export default function Ownerlogin() {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-background-light dark:bg-background-dark">
+      <div className="flex flex-1 items-center justify-center p-4 py-10 sm:p-6 lg:p-8">
+        <div className="flex w-full max-w-md flex-col items-center gap-6">
+          <OwnerloginHeader />
+
+          <OwnerloginForm />
+
+          <OwnerloginFooter />
+        </div>
+      </div>
+    </div>
+  );
+}
