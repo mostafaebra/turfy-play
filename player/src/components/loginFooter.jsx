b8cc3c322ec506769ab4fea686abda1125f76401
yomna <yomna7250@gmail.com>
2025-12-14 22:32:45 +0200

    Reapply "player : login&signup"
    This reverts commit 42eaa45de24273e72ace8454b7ffae2a3ecc5e68.

---
+++ player/src/components/loginFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LoginFooter() {
+  return ( 
+    <p className="text-sm text-center text-text-light mt-6 font-display">
+      New to Turfy Play?{" "}
+      {/* */}
+      <span className="text-secondary font-medium hover:underline cursor-pointer">
+        Create an account
+      </span>
+    </p>
+  );
+}
--- player/src/pages/login/login.jsx
+++ player/src/pages/login/login.jsx
@@ -1,4 +1,19 @@
-const Login = () => {
-  return <h1>Login Page</h1>;
-};
-export default Login;
+import React from "react";
+import Header from "../../components/Header";
+import LoginForm from "../../components/LoginForm";
+import LoginFooter from "../../components/loginFooter";
+
+
+export default function Login() {
+  return (
+    
+    <div className="min-h-screen flex items-center justify-center bg-light-gray px-4 font-display">
+      {/* استخدام rounded-xl للحواف الكبيرة المخصصة */}
+      <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-8">
+        <Header />
+        <LoginForm />
+        <LoginFooter />
+      </div>
+    </div>
+  );
+}
